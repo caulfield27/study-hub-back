eca9ed6a7864565d6cefe55f51eb0f67bdc5f969
holden <al1onggwp@gmail.com>
2025-12-10 16:47:36 +0500

    update database pool

--- app/db/database.js
+++ app/db/database.js
@@ -1,11 +1,8 @@
 const {Pool} = require("pg");
+const { DATABASE_CONNECTION } = require("../utils/getEnv");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "playground",
-    password: "",
-    port: 5432
+    connectionString: DATABASE_CONNECTION
 });
 
 module.exports = pool;
--- app/utils/getEnv.js
+++ app/utils/getEnv.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT,
   BASE_URL: process.env.BASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  DATABASE_CONNECTION: process.env.DATABSE_CONNECTION
 };
