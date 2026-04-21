48b8a98eb212fbf9bfe0e4728698b3248de5eb3f
caulfield27 <al1onggwp@gmail.com>
2026-04-21 11:30:29 +0500

    add logs

--- app/db/database.js
+++ app/db/database.js
@@ -1,6 +1,7 @@
 const {Pool} = require("pg");
 const { DATABASE_CONNECTION } = require("../utils/getEnv");
 
+console.log('connection string: ', DATABASE_CONNECTION)
 const pool = new Pool({
     connectionString: DATABASE_CONNECTION
 });
