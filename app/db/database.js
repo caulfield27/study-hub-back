c478675915f5d9a3f502251a30b163a6b0923098
caulfield27 <al1onggwp@gmail.com>
2026-04-21 11:42:12 +0500

    fix: env lose issue

--- app/db/database.js
+++ app/db/database.js
@@ -1,9 +1,8 @@
 const {Pool} = require("pg");
 const { DATABASE_CONNECTION } = require("../utils/getEnv");
 
-console.log('connection string: ', DATABASE_CONNECTION, process.env.DATABASE_CONNECTION)
 const pool = new Pool({
-    connectionString: DATABASE_CONNECTION
+    connectionString: process.env.DATABASE_CONNECTION
 });
 
 module.exports = pool;
