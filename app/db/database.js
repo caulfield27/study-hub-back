d7ce4e5b18c435f75b9fb3984af042099cf7ee04
caulfield27 <al1onggwp@gmail.com>
2026-04-21 11:37:17 +0500

    debugging

--- app/db/database.js
+++ app/db/database.js
@@ -1,7 +1,7 @@
 const {Pool} = require("pg");
 const { DATABASE_CONNECTION } = require("../utils/getEnv");
 
-console.log('connection string: ', DATABASE_CONNECTION)
+console.log('connection string: ', DATABASE_CONNECTION, process.env.DATABASE_CONNECTION)
 const pool = new Pool({
     connectionString: DATABASE_CONNECTION
 });
