c4852c845f5d7d9592d73205d9f741827cbf827e
caulfield27 <al1onggwp@gmail.com>
2026-05-01 15:29:53 +0500

    update course model

--- app/models/courses.model.js
+++ app/models/courses.model.js
@@ -2,7 +2,7 @@ const pool = require("../db/database");
 
 async function selectCourses() {
   try {
-    const query = `
+    const courseQuery = `
         SELECT (
         id,
         poster,
@@ -11,14 +11,20 @@ async function selectCourses() {
         language,
         duration,
         is_free,
-        price,
-        course_categories
+        price
         ) FROM courses`;
-    const result = await pool.query(query);
-    return result.rows;
+    const categoryQuery = `
+      SELECT co.id AS course_id, c.name AS category_name
+      FROM courses co
+      JOIN course_categories cc ON co.id = cc.course_id
+      JOIN categories c ON c.id = cc.category_id
+    `;
+    const courses = await pool.query(courseQuery);
+    const categories = await pool.query(categoryQuery);
+    return [...courses.rows, categories.row];
   } catch (e) {
     throw e;
   }
 }
 
-module.exports = {selectCourses};
+module.exports = { selectCourses };
